82352c4adcf6f5f46f8698ec1ce826c7fc35fbe9
jhyungit <jh021199@gmail.com>
2023-01-18 21:24:00 +0900

    git commit

---
+++ lv2_이진 변환 반복하기.py
@@ -0,0 +1,18 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/70129
+
+def solution(s):
+    answer = ""
+    rotation = 0
+    cnt_zero = 0
+    while True:
+        for i in s:
+            if i == "1":
+                answer += "".join(i)
+            else:
+                cnt_zero += 1
+        s = bin(len(answer))[2:]
+        answer = ""
+        rotation += 1
+        if s == "1":
+            break
+    return [rotation,cnt_zero]
