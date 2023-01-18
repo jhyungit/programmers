679bd334ffaab809cf5d91c4e74300fd72ba0228
jhyungit <jh021199@gmail.com>
2023-01-18 18:09:26 +0900

    git commit

---
+++ lv2_올바른 괄호.py
@@ -0,0 +1,11 @@
+# # https://school.programmers.co.kr/learn/courses/30/lessons/12909
+
+# def solution(s):
+#     answer = True
+
+#     return True
+
+x = [1,2,3]
+y = [5,6,7]
+for i, j in zip(x,y):
+    print(i,j)
---
+++ lv2_올바른괄호.py
@@ -0,0 +1,14 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/12909
+
+def solution(s):
+    x = []
+    
+    for i in s:
+        if i == "(":
+            x.append(i)
+        else:
+            if x == []:
+                return False
+            else:
+                x.pop()
+    return x == []
