0875b542a012cd743fd38dc68c221304d2356626
jhyungit <jh021199@gmail.com>
2023-01-25 21:40:58 +0900

    coding test

---
+++ lv2_카펫.py
@@ -0,0 +1,8 @@
+def solution(brown, yellow):
+    total = brown + yellow
+    for b in range(3, total+1):
+        if total%b == 0:
+            a = total/b
+            if a >= b:
+                if 2*a + 2*b == brown + 4:
+                    return [a,b]
