b51c25bfdbbb186d982bf11492cc8f6d407b16fb
jhyungit <jh021199@gmail.com>
2023-02-01 11:31:21 +0900

    coding test

---
+++ lv2_H-Index.py
@@ -0,0 +1,18 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/42747
+
+def solution(citations):
+    h = len(citations)
+    while h >= 0:
+        gte = 0
+        lte = 0
+        for c in citations:
+            if c >= h:
+                gte += 1
+            else:
+                lte += 1
+        
+        if gte >= h and lte <= h:
+            return h
+        h -= 1
+
+solution([3, 0, 6, 1, 5, ])
