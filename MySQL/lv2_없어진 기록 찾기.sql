e1162c5fb2bcfe769c91840656decfb2124f95a2
jhyungit <jh021199@gmail.com>
2024-01-09 18:59:58 +0900

    sql commit

---
+++ MySQL/lv2_없어진 기록 찾기.sql
@@ -0,0 +1,9 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59042
+
+-- 코드를 입력하세요
+SELECT O.ANIMAL_ID, O.NAME
+FROM ANIMAL_INS I
+    RIGHT JOIN ANIMAL_OUTS O
+    ON I.ANIMAL_ID = O.ANIMAL_ID
+WHERE I.ANIMAL_ID IS NULL
+ORDER BY O.ANIMAL_ID, O.NAME
