03cf4256593b0a5bedfa023040fb9cf4604d2acb
jhyungit <jh021199@gmail.com>
2025-03-04 22:38:52 +0900

    commit

---
+++ MySQL/lv3_부서별 평균 연봉 조회하기.sql
@@ -0,0 +1,8 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/284529
+
+SELECT D.DEPT_ID, D.DEPT_NAME_EN, ROUND(AVG(SAL)) AS AVG_SAL
+FROM HR_DEPARTMENT D
+    JOIN HR_EMPLOYEES E
+    ON D.DEPT_ID = E.DEPT_ID
+GROUP BY 1
+ORDER BY 3 DESC
