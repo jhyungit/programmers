f108a3e5a353993fc4957c977bf64f8f781fd56a
jhyungit <jh021199@gmail.com>
2025-03-04 22:29:37 +0900

    commit

---
+++ others/lv3_여행경로.py
@@ -0,0 +1,32 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/43164
+
+from collections import deque,defaultdict
+
+def solution(tickets):
+    depth = len(tickets) + 1
+    
+    for ticket in tickets:
+        ticket.append(False)
+
+    tickets.sort(key = lambda x : x[1])
+    way = ['ICN']
+    
+    def dfs(airport):
+        for ticket in tickets:
+            start, end, visited = ticket
+            
+            if start == airport and not visited:
+                way.append(end)
+                ticket[2] = True # 방문 처리
+                
+                if depth == len(way) or dfs(ticket[1]):
+                    return way
+                
+                ticket[2] = False # 탐색했는데, 경로가 다 안 찼으면
+                way.pop()
+                
+    answer = dfs('ICN')
+    
+    return answer
+
+solution([["ICN", "SFO"], ["ICN", "ATL"], ["SFO", "ATL"], ["ATL", "ICN"], ["ATL","SFO"]])
