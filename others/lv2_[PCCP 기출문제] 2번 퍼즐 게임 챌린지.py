61c6a5b89da46f69e29d4f4952ad4ebb4f931e0c
jhyungit <jh021199@gmail.com>
2025-09-04 18:33:11 +0900

    commit

---
+++ others/lv2_[PCCP 기출문제] 2번 퍼즐 게임 챌린지.py
@@ -0,0 +1,28 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/340212
+
+#     diff        time_cur       time_prev
+# 현재 퍼즐 난이도   현재 퍼즐 소요 시간  이전 퍼즐 소요시간
+
+def solution(diffs, times, limit):
+    result = set()
+    left, right = 1, max(diffs)
+    
+    while left <= right:
+        answer = 0
+        mid = (left + right) // 2
+        
+        for i, diff in enumerate(diffs):
+            if mid >= diff:
+                answer += times[i]
+            else:
+                cnt = diff - mid
+                if i != 0:
+                    answer += (times[i] + times[i-1]) * cnt + times[i]
+        if answer <= limit:
+            result.add(mid)
+            right = mid - 1
+            
+        else:
+            left = mid + 1
+    
+    return min(result)
